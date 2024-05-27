8a2fe3132f0983404383a2a0a59b1b143a648b7d
fredasabrinaputri <161544712+fredasabrinaputri@users.noreply.github.com>
2024-05-28 01:17:31 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda"
+  "FLUTTER_TARGET=C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNTQ1Zjg3MDVkZjMwMTg3N2Q3ODcxMDdiYWMxYTZlOWZjOWVlMWFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\tugas_lab_xipplg1\\tugas bu yaya\\exam_preda\\.dart_tool\\package_config.json"
+)
